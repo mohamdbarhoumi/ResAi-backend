5180f83e5a2fcd12346368f316b23f963a25d900
mohamdbarhoumi <barhoumimohamed@fsg.u-gabes.tn>
2025-11-15 14:01:00 +0100

    Cors fix

--- src/main/java/org/example/resai/config/SecConfig.java
+++ src/main/java/org/example/resai/config/SecConfig.java
@@ -65,7 +65,7 @@ public class SecConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000","https://resumai-tan.vercel.app"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
